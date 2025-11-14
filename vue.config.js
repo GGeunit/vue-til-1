4c80698e425670e8740e9c296c909d6e140a386e
GGeunit <165870924+GGeunit@users.noreply.github.com>
2025-11-14 16:31:50 +0900

    update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-	devServer: {
-		overlay: false,
-	},
+  devServer: {
+    overlay: false,
+  },
+  // 빌드(프로덕션)에서도 소스맵 보고 싶으면
+  productionSourceMap: true,
+
+  // webpack devtool 설정
+  configureWebpack: {
+    devtool: 'source-map', // 또는 'eval-source-map'
+  },
 };
