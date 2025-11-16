458e4ce477471c3e941f710eb89800add9d9b37d
GGeunit <165870924+GGeunit@users.noreply.github.com>
2025-11-17 02:49:05 +0900

    update

--- src/components/LoginForm.spec.js
+++ src/components/LoginForm.spec.js
@@ -1,3 +1,31 @@
-import LoginForm from '@/LoginForm.vue';
+import { shallowMount } from '@vue/test-utils';
+import LoginForm from './LoginForm.vue';
 
-describe('LoginForm.vue', () => {});
+describe('LoginForm.vue', () => {
+  test('ID는 이메일 형식이 아니면 경고 메시지가 출력된다', () => {
+    const wrapper = shallowMount(LoginForm, {
+      data() {
+        return {
+          username: 'test',
+        };
+      },
+    });
+    const wraningText = wrapper.find('.warning');
+    console.log(wraningText.html());
+    expect(wraningText.exists()).toBeTruthy();
+  });
+
+  test('ID와 PW가 입력되지 않으면 로그인 버튼이 비활성화 된다', () => {
+    const wrapper = shallowMount(LoginForm, {
+      data() {
+        return {
+          username: '',
+          password: '',
+        };
+      },
+    });
+    // wrapper.find('.btn');
+    const button = wrapper.find('button');
+    expect(button.element.disabled).toBeTruthy();
+  });
+});
